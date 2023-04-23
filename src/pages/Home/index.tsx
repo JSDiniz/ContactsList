fdef04a99513bcd10535ad44d8869d4bf3d146d7
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-23 20:09:56 -0300

    feat: pages Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,12 @@
+import { VStack } from "@chakra-ui/react";
+import Header from "../../components/Header";
+
 const Home = () => {
-  return <div>Testo</div>;
+  return (
+    <VStack>
+      <Header />
+    </VStack>
+  );
 };
 
 export default Home;
