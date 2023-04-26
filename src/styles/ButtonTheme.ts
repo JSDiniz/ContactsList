c9cc4e8dfeaf865f9dabe0131bb145b6512ab153
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-26 12:12:29 -0300

    style: styles ButtonTheme

--- src/styles/ButtonTheme.ts
+++ src/styles/ButtonTheme.ts
@@ -1,17 +1,18 @@
 import { defineStyleConfig } from "@chakra-ui/react";
 
-export const ButtonTheme = defineStyleConfig({
-  // style object for base or default style
+const ButtonTheme = defineStyleConfig({
   baseStyle: {},
-  // styles for different sizes ("sm", "md", "lg")
+
   sizes: {},
-  // styles for different visual variants ("outline", "solid")
+
   variants: {
     primary: {
-      bg: "primary_200",
-      color: "primary_100",
+      bg: "primary_cor",
+      color: "whiteFixed",
     },
   },
-  // default values for 'size', 'variant' and 'colorScheme'
+
   defaultProps: {},
 });
+
+export default ButtonTheme;
