c9d32d9683bc312e8826573317be57b227e4fb28
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-26 12:12:52 -0300

    feat: components Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,15 @@
-import {
-  Flex,
-  Text,
-  HStack,
-  useColorMode,
-  useColorModeValue,
-  Button,
-  DarkMode,
-} from "@chakra-ui/react";
+import { Flex, Text, HStack, useColorMode, Button } from "@chakra-ui/react";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Flex h="100vh" w="100vw" align="center" justify="center">
+    <Flex align="center" justify="center">
       <HStack gap="5">
         <Button onClick={toggleColorMode} variant="primary">
           Teste
         </Button>
+        <Text color={"primary_cor"}>Testando</Text>
       </HStack>
     </Flex>
   );
