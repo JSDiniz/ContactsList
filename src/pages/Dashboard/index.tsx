458318e8e601e799e356bed593dda0db61a81cd3
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-27 22:44:48 -0300

    feat: pages Dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { VStack } from "@chakra-ui/layout";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Dashboard;
