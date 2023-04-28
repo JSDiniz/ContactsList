521618574fcc4f54ee5b2b0bddeca8700a7e0c4c
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-27 22:45:45 -0300

    feat: components Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,48 @@
-import { Flex, Text, HStack, useColorMode, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  useColorMode,
+  UnorderedList,
+  ListItem,
+  Box,
+  Link,
+} from "@chakra-ui/react";
+import Section from "../../styles/ContainerTheme";
+import { FaRegAddressBook, FaMoon } from "react-icons/fa";
+import { BiSun } from "react-icons/bi";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const list = ["Home", "Login", "Cadastro"];
 
   return (
-    <Flex align="center" justify="center">
-      <HStack gap="5">
-        <Button onClick={toggleColorMode} variant="primary">
-          Teste
-        </Button>
-        <Text color={"primary_cor"}>Testando</Text>
-      </HStack>
-    </Flex>
+    <Section h="80px">
+      <Flex alignItems="center" gap="14px" h="100%">
+        <FaRegAddressBook size="40px" rotate="-90" />
+        <Text variant="Heading-3-500">Contacts List</Text>
+      </Flex>
+      <Flex alignItems="center" h="100%" gap="14px">
+        <UnorderedList w="100%" listStyleType="none" display="flex" gap="14px">
+          {list.map((element: string) => (
+            <ListItem>
+              <Link textDecor="none">{element}</Link>
+            </ListItem>
+          ))}
+        </UnorderedList>
+        <Box w="30px" cursor="pointer" onClick={toggleColorMode}>
+          {colorMode == "dark" ? (
+            <FaMoon size="24px" />
+          ) : (
+            <BiSun size="30px" color="#F2C94C" />
+          )}
+        </Box>
+      </Flex>
+    </Section>
   );
 };
 
 export default Header;
+
+// {list?.map((item: any) => {
+//   <ListItem>{item}</ListItem>;
+// })}
