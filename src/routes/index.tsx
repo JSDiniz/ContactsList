c3d719c1a5a0966d2b7f0977b0fbeabf2d4a8fc0
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-30 00:12:50 -0300

    feat: routes index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
 
 const Routed = () => {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
