53cc71f6bd672ebcddd31a450ba31abb8ac99a64
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-29 21:10:49 -0300

    refactor: styles ContainerTheme

--- src/styles/ContainerTheme.ts
+++ src/styles/ContainerTheme.ts
@@ -3,12 +3,9 @@ import { chakra } from "@chakra-ui/react";
 const Section = chakra("section", {
   baseStyle: {
     maxW: "1024px",
-    w: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    boxSizing: "border-box",
-    m: { base: "0 10px", md: "0 30px", lg: "0" },
+    w: "full",
+    m: { base: "0 10px", md: "0 20px", xl: "0" },
+    overflow: "hidden",
   },
 });
 
