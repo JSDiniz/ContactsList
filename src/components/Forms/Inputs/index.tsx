1003a7d84c723390d7a57b8a4c319c8d6efe078f
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-30 23:39:24 -0300

    fix: components Forms Inputs

--- src/components/Forms/Inputs/index.tsx
+++ src/components/Forms/Inputs/index.tsx
@@ -5,8 +5,14 @@ import {
   InputRightElement,
   Icon,
 } from "@chakra-ui/react";
-import { EmailIcon, LockIcon } from "@chakra-ui/icons";
-import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
+import {
+  EmailIcon,
+  LockIcon,
+  PhoneIcon,
+  ViewOffIcon,
+  ViewIcon,
+} from "@chakra-ui/icons";
+import { AiOutlineUser, AiOutlineCamera } from "react-icons/ai";
 import { IInputs } from "../intex";
 
 const Inputs = ({ type, placeholder }: IInputs) => {
@@ -15,20 +21,41 @@ const Inputs = ({ type, placeholder }: IInputs) => {
       <InputLeftElement
         pointerEvents="none"
         children={
-          type === "email" ? (
-            <EmailIcon color="brand1" />
-          ) : (
-            <LockIcon color="brand1" />
+          type === "text" && (
+            <Icon as={AiOutlineUser} color="brand1" boxSize={5} />
           )
         }
       />
+
+      <InputLeftElement
+        pointerEvents="none"
+        children={type === "email" && <EmailIcon color="brand1" />}
+      />
+
+      <InputLeftElement
+        pointerEvents="none"
+        children={type === "password" && <LockIcon color="brand1" />}
+      />
+
+      <InputLeftElement
+        pointerEvents="none"
+        children={type === "tel" && <PhoneIcon color="brand1" />}
+      />
+
+      <InputLeftElement
+        pointerEvents="none"
+        children={
+          type === "url" && (
+            <Icon as={AiOutlineCamera} color="brand1" boxSize={5} />
+          )
+        }
+      />
+
       <Input type={type} placeholder={placeholder} />
       {type === "password" && (
         <InputRightElement
           cursor={"pointer"}
-          children={
-            <Icon as={AiOutlineEyeInvisible} boxSize={5} color="brand1" />
-          }
+          children={<ViewOffIcon color="brand1" />}
         />
       )}
     </InputGroup>
