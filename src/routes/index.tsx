acf4a2ea3ce90a06f3591feadbf4f7227de0eb5b
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-02 19:27:25 -0300

    feat: routes Dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
 
 const Routed = () => {
   return (
@@ -9,6 +10,7 @@ const Routed = () => {
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
+      <Route path="/dashboard" component={Dashboard} />
     </Switch>
   );
 };
