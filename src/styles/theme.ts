efa211c9929af9becf24519284ab3f9572c0ae4b
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-01 23:52:44 -0300

    style: styles theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,10 +2,16 @@ import { extendTheme } from "@chakra-ui/react";
 import cuttonTheme from "./ButtonTheme";
 
 const theme = extendTheme({
+  fonts: {
+    heading: '"IBM Plex Sans", sans-serif;',
+    body: '"Inter", sans-serif',
+  },
+
   config: {
     initialColorMode: "dark",
     useSystemColorMode: false,
   },
+
   semanticTokens: {
     colors: {
       primary_bg: {
@@ -23,6 +29,7 @@ const theme = extendTheme({
       },
     },
   },
+
   colors: {
     yellow: {
       400: "#FDBF50",
@@ -51,6 +58,65 @@ const theme = extendTheme({
       700: "#0A0A0B",
     },
   },
+
+  fontSizes: {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    md: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+    "6xl": "3.75rem",
+    "7xl": "4.5rem",
+    "8xl": "6rem",
+    "9xl": "8rem",
+  },
+
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+
+  lineHeights: {
+    normal: "normal",
+    none: 1,
+    shorter: 1.25,
+    short: 1.375,
+    base: 1.5,
+    tall: 1.625,
+    taller: "2",
+    "3": ".75rem",
+    "4": "1rem",
+    "5": "1.25rem",
+    "6": "1.5rem",
+    "7": "1.75rem",
+    "8": "2rem",
+    "9": "2.25rem",
+    "10": "2.5rem",
+  },
+
+  radii: {
+    none: "0",
+    sm: "0.125rem",
+    base: "0.25rem",
+    md: "0.375rem",
+    lg: "0.5rem",
+    xl: "0.75rem",
+    "2xl": "1rem",
+    "3xl": "1.5rem",
+    full: "9999px",
+  },
+
   components: {
     Button: cuttonTheme,
     Text: {
@@ -107,56 +173,6 @@ const theme = extendTheme({
     },
   },
 
-  fonts: {
-    heading: '"IBM Plex Sans", sans-serif;',
-    body: '"Inter", sans-serif',
-  },
-
-  fontSizes: {
-    xs: "0.75rem",
-    sm: "0.875rem",
-    md: "1rem",
-    lg: "1.125rem",
-    xl: "1.25rem",
-    "2xl": "1.5rem",
-    "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
-    "6xl": "3.75rem",
-    "7xl": "4.5rem",
-    "8xl": "6rem",
-    "9xl": "8rem",
-  },
-
-  fontWeights: {
-    hairline: 100,
-    thin: 200,
-    light: 300,
-    normal: 400,
-    medium: 500,
-    semibold: 600,
-    bold: 700,
-    extrabold: 800,
-    black: 900,
-  },
-
-  lineHeights: {
-    normal: "normal",
-    none: 1,
-    shorter: 1.25,
-    short: 1.375,
-    base: 1.5,
-    tall: 1.625,
-    taller: "2",
-    "3": ".75rem",
-    "4": "1rem",
-    "5": "1.25rem",
-    "6": "1.5rem",
-    "7": "1.75rem",
-    "8": "2rem",
-    "9": "2.25rem",
-    "10": "2.5rem",
-  },
   styles: {
     global: {
       "html, body": {
