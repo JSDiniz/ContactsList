5f5e7f116de1c31e8e88b6f94e57965012563040
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-02 19:16:05 -0300

    feat: pages Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import Section from "../../styles/ContainerTheme";
+import Header from "../../components/Header";
+import Main from "./Main/intex";
+import Footer from "../../components/Footer";
+
+const Home = () => {
+  return (
+    <Section>
+      <Header />
+      <Main />
+      <Footer />
+    </Section>
+  );
+};
+
+export default Home;
