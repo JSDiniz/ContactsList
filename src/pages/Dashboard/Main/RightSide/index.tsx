048b6d0ffa42310f98292c0fd9b363a916f4706a
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-04 12:07:21 -0300

    feat: pages Dashboard Main RightSide

---
+++ src/pages/Dashboard/Main/RightSide/index.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react";
+import Cards from "../../../../components/Cards";
+
+const RightSide = () => {
+  return (
+    <Flex>
+      <Cards />
+    </Flex>
+  );
+};
+
+export default RightSide;
