1d5fba10635c0530db26bc7f39c24be086bb78d9
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-04 18:32:46 -0300

    feat: components Modal ModalError

---
+++ src/components/Modal/ModalError.tsx
@@ -0,0 +1,63 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Text,
+  Center,
+  Box,
+} from "@chakra-ui/react";
+import { FaExclamation } from "react-icons/fa";
+
+interface ModalErrorProps {
+  isOpen: boolean;
+  onClose: () => void;
+  error: string;
+  secondaryTex: string;
+}
+
+const ModalError = ({
+  isOpen,
+  onClose,
+  error,
+  secondaryTex,
+}: ModalErrorProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader display={"flex"}>
+          <Center
+            bg={"red.600"}
+            w={"30px"}
+            h={"30px"}
+            borderRadius={"5px"}
+            mr={1}
+          >
+            <FaExclamation />
+          </Center>
+          <Text>Oops!</Text>
+        </ModalHeader>
+        <ModalCloseButton bg={"red.600"} />
+        <ModalBody textAlign={"center"}>
+          <Text>Ocorreu algum erro! {error}</Text>
+        </ModalBody>
+
+        <ModalFooter display={"column"}>
+          <Button bg={"red.600"} w={"full"} h={"60px"} mr={3} onClick={onClose}>
+            Tentar novamente
+          </Button>
+          <Text mt={"4"} textAlign={"center"}>
+            <Box dangerouslySetInnerHTML={{ __html: secondaryTex }} />
+          </Text>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default ModalError;
