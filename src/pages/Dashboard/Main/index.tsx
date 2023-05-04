caaa442ac2a70c31d11e92807d0ffae9abcc6736
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-04 16:41:08 -0300

    feat: pages Dashboard Main

--- src/pages/Dashboard/Main/index.tsx
+++ src/pages/Dashboard/Main/index.tsx
@@ -1,12 +1,15 @@
 import { Flex } from "@chakra-ui/react";
 import LeftSide from "./LeftSide";
 import RightSide from "./RightSide";
+import { useAuth } from "../../../contexts/Auth";
 
 const Main = () => {
+  const { user } = useAuth();
+
   return (
     <Flex as={"main"} minH="calc(100vh - 5rem - 3.125rem)" flexDir={"column"}>
       <LeftSide />
-      <RightSide />
+      {user.contacts.length !== 0 ? <RightSide /> : "Adicionar contatcs"}
     </Flex>
   );
 };
