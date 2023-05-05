142efca1568ff2f08de811f7b6f86299a82214dc
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-05 11:11:49 -0300

    feat: pages Dashboard Main LeftSide

--- src/pages/Dashboard/Main/LeftSide/index.tsx
+++ src/pages/Dashboard/Main/LeftSide/index.tsx
@@ -1,20 +1,37 @@
-import { Button, Flex, HStack, Heading, Icon } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  Icon,
+  useDisclosure,
+} from "@chakra-ui/react";
 import Search from "../../../../components/Search";
+import CreateContacts from "../../../../components/CreateContacts";
 
 const LeftSide = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
-    <Flex
-      w={"full"}
-      alignItems={"center"}
-      gap={4}
-      flexDir={["column", "column", "row", "row"]}
-      justifyContent={"flex-end"}
-    >
-      <Search />
-      <Button fontSize={"sm"} w={["full", "auto", "auto", "auto"]}>
-        Adicionar Contacts
-      </Button>
-    </Flex>
+    <>
+      {isOpen && <CreateContacts isOpen={isOpen} onClose={onClose} />}
+      <Flex
+        w={"full"}
+        alignItems={"center"}
+        gap={4}
+        flexDir={["column", "column", "row", "row"]}
+        justifyContent={"flex-end"}
+      >
+        <Search />
+        <Button
+          onClick={onOpen}
+          fontSize={"sm"}
+          w={["full", "auto", "auto", "auto"]}
+        >
+          Adicionar Contacts
+        </Button>
+      </Flex>
+    </>
   );
 };
 
