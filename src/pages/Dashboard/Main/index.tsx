defd8514acc2b6eb5acf2ec39a69ada489b0eae4
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-09 10:47:30 -0300

    perf: pages Dashboard main

--- src/pages/Dashboard/Main/index.tsx
+++ src/pages/Dashboard/Main/index.tsx
@@ -2,14 +2,24 @@ import { Flex } from "@chakra-ui/react";
 import LeftSide from "./LeftSide";
 import RightSide from "./RightSide";
 import { useAuth } from "../../../contexts/Auth";
+import { useEffect, useState } from "react";
+import { useContacts } from "../../../contexts/Contact";
 
 const Main = () => {
-  const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const { user, token, contacts } = useAuth();
+  const { loadContacts } = useContacts();
+
+  useEffect(() => {
+    loadContacts(user.id, token).then((res) => {
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <Flex as={"main"} minH="calc(100vh - 5rem - 3.125rem)" flexDir={"column"}>
       <LeftSide />
-      {user.contacts.length !== 0 ? <RightSide /> : "Adicionar contatcs"}
+      {contacts.length !== 0 ? <RightSide /> : "Adicionar contatcs"}
     </Flex>
   );
 };
