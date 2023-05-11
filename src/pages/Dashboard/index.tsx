5755cbb43b0e7273221243d6010d828f5eb846ab
JunielsonDiniz <juniosantos_@hotmail.com>
2023-05-11 19:15:27 -0300

    style: pages Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
+import Main from "./Main";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import Main from "./Main";
 import Section from "../../styles/ContainerTheme";
 
 const Dashboard = () => {
