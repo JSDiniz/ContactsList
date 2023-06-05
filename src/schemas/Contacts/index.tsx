a521371d6399b3d2f390c6b329cc0c605feffc74
Diniz <juniosantos_@hotmail.com>
2023-06-05 04:10:56 -0300

    feat: schemas Contacts

--- src/schemas/Contacts/index.tsx
+++ src/schemas/Contacts/index.tsx
@@ -4,17 +4,23 @@ import { ICreateContacts } from "../../interface/Contacts";
 
 const contactsSchemas: SchemaOf<ICreateContacts> = yup.object().shape({
   name: yup.string().max(60).required("Campo obrigatório"),
-  telephone: yup
+  phone: yup
     .string()
     .max(11)
     .required("Campo obrigatório")
-    .matches(/^[0-9]([0-9]{8}|[0-9]{9})/, "Must be a valid phone number"),
+    .matches(
+      /^[0-9]([0-9]{8}|[0-9]{9})/,
+      "Deve ser um número de telefone válido"
+    ),
   email: yup
     .string()
-    .email("Must be a valid email")
+    .email("Deve ser um e-mail válido")
     .max(60)
     .required("Campo obrigatório"),
   imageUrl: yup.string().notRequired(),
+  githubUrl: yup.string().notRequired(),
+  linkedinUrl: yup.string().notRequired(),
+  portfolioUrl: yup.string().notRequired(),
 });
 
 export default contactsSchemas;
