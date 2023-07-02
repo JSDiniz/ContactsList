0d160302cf15f51a57e81a197f8a0b60f8910cd4
Diniz <juniosantos_@hotmail.com>
2023-07-01 23:10:48 -0300

    feat: add logic if you have contact

--- src/pages/Dashboard/Main/index.tsx
+++ src/pages/Dashboard/Main/index.tsx
@@ -1,12 +1,21 @@
+import ModalContact from "../../../components/Modal/ModalContact";
+import { useAuth } from "../../../contexts/Auth";
 import LeftSide from "./LeftSide";
 import RightSide from "./RightSide";
 import { Flex } from "@chakra-ui/react";
 
 const Main = () => {
+  const { contactsCopy } = useAuth();
   return (
     <Flex as={"main"} minH="calc(100vh - 5rem - 3.125rem)" flexDir={"column"}>
-      <LeftSide />
-      <RightSide />
+      {contactsCopy.length !== 0 ? (
+        <>
+          <LeftSide />
+          <RightSide />
+        </>
+      ) : (
+        <ModalContact />
+      )}
     </Flex>
   );
 };
