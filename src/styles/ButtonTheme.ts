434c4918f8a1eadd2aa6f911110eb6a72771217a
Diniz <juniosantos_@hotmail.com>
2024-02-02 19:25:30 -0300

    refactor: primary on ButtonTheme

--- src/styles/ButtonTheme.ts
+++ src/styles/ButtonTheme.ts
@@ -6,6 +6,18 @@ const cuttonTheme = defineStyleConfig({
   sizes: {},
 
   variants: {
+    primary: {
+      p: "20px 25px",
+      w: ["full", "unset", "unset", "unset"],
+      bg: "orange.600",
+      color: "grey.100",
+      fontSize: "16px",
+      fontWeight: "normal",
+      border: "1px ",
+      borderColor: "orange.600",
+      _hover: { bg: "transparent", color: "orange.600" },
+    },
+
     toEnter: {
       w: "262px",
       bg: "orange.600",
@@ -34,6 +46,21 @@ const cuttonTheme = defineStyleConfig({
       border: "1px ",
       borderColor: "grey.400",
       _hover: { bg: "red.600", color: "grey.100", borderColor: "red.600" },
+    },
+
+    NotFound: {
+      p: "30px 25px",
+      w: ["full", "unset", "unset", "unset"],
+      color: "secondary_text",
+      fontSize: "16px",
+      fontWeight: "700",
+      border: "1px ",
+      borderColor: "secondary_text",
+      _hover: {
+        bg: "secondary_text",
+        color: "NotFound_text",
+        borderColor: "secondary_text",
+      },
     },
   },
 
