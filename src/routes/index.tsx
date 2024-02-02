c1224ae70384d13067457667735a1ee16cef1c96
Diniz <juniosantos_@hotmail.com>
2024-02-02 19:25:02 -0300

    refactor: Routed on Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,14 +4,19 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import { Switch } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
+import { PageNotFound } from "../pages/PageNotFound";
+import { useAuth } from "../contexts/Auth";
 
 const Routed = () => {
+  const { token } = useAuth();
+
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route component={PageNotFound} isPrivate={!!token} />
     </Switch>
   );
 };
